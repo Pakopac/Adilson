586fc74dbc34103535857350893562691a88d43b
Pakopac <lilian.pacaud@supinternet>
2019-02-06 19:54:34 +0100

    Added application and partners parts

--- assets/script.js
+++ assets/script.js
@@ -27,6 +27,14 @@ window.onload = function () {
       }
   }
 
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 1000);
+    });
+
 };
 
 function changeSwitchState(stateIndustry, stateParticulier) {
