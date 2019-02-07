109c7b309107b310224bb46bbe2063f9236586ff
Pakopac <lilian.pacaud@supinternet>
2019-02-07 02:18:30 +0100

    Change effect on switch

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,28 @@
 var choiceIndustry = document.querySelector('.choice-industry');
-var choiceParticulier = document.querySelector('.choice-particulier');
+var choiceIndividual = document.querySelector('.choice-individual');
 var switchIndustry = document.querySelector('.switch-industry');
-var switchParticulier = document.querySelector('.switch-particulier');
+var switchIndividual = document.querySelector('.switch-individual');
+var titleIndividual = document.querySelector('.titleIndividual')
+var titleIndusty = document.querySelector('.titleIndustry')
 
 var labelSwitch = document.querySelector('.checkSwitch');
 
 window.onload = function () {
-    labelSwitch.checked = false
+    labelSwitch.checked = false;
 
   choiceIndustry.onclick = () => {
-      changeSwitchState('block','none');
-          labelSwitch.checked = false
+      labelSwitch.checked = true;
+      changeSwitchState();
   };
 
-  choiceParticulier.onclick = () => {
-      changeSwitchState('none','block');
-          labelSwitch.checked = true
-  }
+  choiceIndividual.onclick = () => {
+      labelSwitch.checked = false;
+      changeSwitchState();
+  };
 
   labelSwitch.onclick = () => {
-      if(labelSwitch.checked == true){
-          changeSwitchState('none','block');
-      }
-      else {
-          changeSwitchState('block','none');
-      }
-  }
+      changeSwitchState()
+  };
 
     $(document).on('click', 'a[href^="#"]', function (event) {
         event.preventDefault();
@@ -37,7 +34,36 @@ window.onload = function () {
 
 };
 
-function changeSwitchState(stateIndustry, stateParticulier) {
-    switchIndustry.style.display = stateIndustry;
-    switchParticulier.style.display = stateParticulier
+function changeSwitchState() {
+    if(labelSwitch.checked === true){
+        choiceIndustry.style.background = 'rgba(0, 0, 0, 0.8)';
+        choiceIndustry.style.justifyContent = 'start';
+        switchIndustry.style.display = 'block';
+        titleIndusty.style.color = '#FFFFFF';
+        titleIndusty.style.background = 'transparent';
+        titleIndusty.style.border = 0;
+
+        choiceIndividual.style.background = 'rgba(0, 0, 0, 0.4)';
+        choiceIndividual.style.justifyContent = 'center';
+        switchIndividual.style.display = 'none';
+        titleIndividual.style.color = '#323335';
+        titleIndividual.style.background = '#FFFFFF';
+        titleIndividual.style.border = '1px solid #6599A0';
+    }
+    else {
+        choiceIndividual.style.background = 'rgba(0, 0, 0, 0.8)';
+        choiceIndividual.style.justifyContent = 'start';
+        switchIndividual.style.display = 'block';
+        titleIndividual.style.color = '#FFFFFF';
+        titleIndividual.style.background = 'transparent';
+        titleIndividual.style.border = 0;
+
+        choiceIndustry.style.background = 'rgba(0, 0, 0, 0.4)';
+        choiceIndustry.style.justifyContent = 'center';
+        switchIndustry.style.display = 'none';
+        switchIndustry.style.display = 'none';
+        titleIndusty.style.color = '#323335';
+        titleIndusty.style.background = '#FFFFFF';
+        titleIndusty.style.border = '1px solid #6599A0';
+    }
 }
